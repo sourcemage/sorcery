sourcemage/sorcery

commit 468486f77bb304a1318dc64a190c1be2ccc504f7

Makefile: version bumping for devel

Makefile
@@ -5,7 +5,7 @@ AWK ?= awk
 help:
 	@${AWK} 'sub(/^#HELP: ?/, "")' ${.MAKE.MAKEFILES}
 
-BRANCH = devel
+BRANCH = $(shell cat etc/sorcery/version)
 release = sorcery-$(BRANCH).tar.bz2
 
 ts-scm = .git/refs/heads/master
@@ -24,7 +24,7 @@ release: $(release)
 #HELP:	bump      - Bump version
 .PHONY: bump
 bump:
-	git describe --tags | >etc/sorcery/version ${AWK} -f bump.awk
+	git log -1 --format=%cs | tr -d '-' >etc/sorcery/version
 
 devinst: bump install
 

etc/sorcery/version
@@ -1,1 +1,1 @@
-20221117
+20240108

commit bca927a6c1b11b0ca3ae102627f88710fa618d69

Fix version bumping
- Revert back to parsing tags, but match version tags only
- Add timestamp (from commit date) to snapshots

Fixes: 468486f77bb3 ("Makefile: version bumping for devel")

Makefile
@@ -27,7 +27,9 @@ release: $(release)
 #HELP:	bump      - Bump version
 .PHONY: bump
 bump:
-	git log -1 --format=%cs | tr -d '-' >etc/sorcery/version
+	git describe --always --tags  --match=v\* | \
+		${AWK} -f bump.awk | \
+		tee etc/sorcery/version
 
 devinst: bump install
 

bump.awk
@@ -9,12 +9,16 @@ BEGIN {
 	for (i = 1; i < NF; i++)
 		$i = $(i+1)
 	--NF
-	print
 	exit
 }
 
-# bump snapshot
+# add placeholder for unknown predecessor
+!/^v/ { sub(/^/, "0-0-g") }
+
+# add timestamp to snapshot
 {
-	sub(/.*-g/, strftime("%Y%m%d-g", systime(), 1))
-	print
+	"git log -1 --format=%ct" | getline  t
+	sub(/-/, strftime("+%Y%m%d-", t, 1))
 }
+
+END { print }